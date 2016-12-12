fbda2cd3a02dd862a323469371e9538990500d00
cartlamy <cart@MacBook-Pro-de-Administrateur.local>
2016-12-12 17:39:30 +0100

    Fix file extension jsx -> js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './containers/App.jsx';
+import App from './containers/App';
 import './index.scss';
 
 ReactDOM.render(<App />, document.getElementById('root'));
