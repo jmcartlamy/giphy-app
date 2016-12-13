12d7f395f73c6e8d5a306b5a753140d1d358d752
Jean-Marie Cart-Lamy <jean-marie.cart-lamy@fullsix.com>
2016-12-13 14:13:51 +0100

    Added onSearchChangedHandler

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchForm = (props) => {
-    return (
-        <form id={props.formId}>
-            <input type="text" name="removeSearch" value="" />
-            <input type="text" name="search" />
-            <input type="submit" value="" />
-        </form>
-    )
+class SearchForm extends Component {
+
+    constructor() {
+        super();
+
+        this.onSearchChangedHandler = this.onSearchChangedHandler.bind(this);
+
+        this.state = {
+            showClearCross: false
+        };
+    }
+
+    onSearchChangedHandler(e) {
+        this.setState({
+            showClearCross: e.currentTarget.value.length > 0
+        });
+    }
+
+
+        this.setState({
+            showClearCross: false
+        });
+
+                <input ref="searchInput" type="text" name="search" onChange={this.onSearchChangedHandler} />
+                <input type="submit" value="&#128269;" />
+            </form>
+        )
+    }
 };
 
 export default SearchForm;
