38faa49ad7b946b1e0413fdbfef67cb03a21b3be
Jean-Marie Cart-Lamy <jean-marie.cart-lamy@fullsix.com>
2016-12-13 14:14:28 +0100

    Added onClickClearCross

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -6,6 +6,7 @@ class SearchForm extends Component {
         super();
 
         this.onSearchChangedHandler = this.onSearchChangedHandler.bind(this);
+        this.onClickClearCross = this.onClickClearCross.bind(this);
 
         this.state = {
             showClearCross: false
@@ -18,10 +19,19 @@ class SearchForm extends Component {
         });
     }
 
+    onClickClearCross() {
+        this.refs.searchInput.value = '';
 
         this.setState({
             showClearCross: false
         });
+    }
+        const { showClearCross } = this.state;
+
+        return (
+                {showClearCross &&
+                    <button type="button" onClick={this.onClickClearCross}>&#9587;</button>
+                }
 
                 <input ref="searchInput" type="text" name="search" onChange={this.onSearchChangedHandler} />
                 <input type="submit" value="&#128269;" />
