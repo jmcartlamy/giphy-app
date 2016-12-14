cd4629f5d37cdf24a041534ef428dc3e26e6bfb5
Jean-Marie Cart-Lamy <jean-marie.cart-lamy@fullsix.com>
2016-12-14 14:54:43 +0100

    Added toggleFav

---
+++ src/components/Gif.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import cs from 'classnames';
+
+class Gif extends Component {
+
+    constructor() {
+        super();
+
+        this.onClickFavHandler = this.onClickFavHandler.bind(this);
+    }
+
+    onClickFavHandler() {
+        this.props.onFavGifClickCallback(this.props.gifId);
+    }
+
+    render() {
+
+        const { gifSrc, gifFav } = this.props;
+
+        const gifCSSClassnames = cs(
+            {
+                'checked': gifFav === true
+            }
+        );
+
+        return (
+            <div>
+                <div>
+                    <span className={gifCSSClassnames} onClick={this.onClickFavHandler}>&#9734;</span>
+                </div>
+                <img src={gifSrc} />
+            </div>
+        )
+    }
+}
+
+export default Gif;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 import cs from 'classnames';
+import Gif from '../components/Gif';
 
 import SearchForm from '../components/SearchForm';
 import Tabs from '../components/Tabs';
@@ -12,12 +13,39 @@ class App extends Component {
 
         this.onSearchChangedHandler = this.onSearchChangedHandler.bind(this);
         this.onClickClearCross = this.onClickClearCross.bind(this);
+        this.onFavGifClick = this.onFavGifClick.bind(this);
+
+        const favs = (typeof(Storage) !== 'undefined' && localStorage.getItem('favourites') !== null) ?
+            JSON.parse(localStorage.getItem('favourites')) :
+            [];
 
         this.state = {
             search: false,
             loading: false,
-            gifElements: []
+            gifsData: [],
+            listFavGifIDs: favs
         }
+    }
+
+    onFavGifClick(idFav) {
+        const currentFavs = this.state.listFavGifIDs;
+        const gifIsFaved = currentFavs.indexOf(idFav) > -1;
+        let newFavs = [];
+
+        if (gifIsFaved) {
+            newFavs = currentFavs.filter(id => id !== idFav);
+        }
+        else {
+            newFavs = currentFavs.concat([idFav]);
+        }
+
+        console.log(newFavs);
+
+        this.setState({
+            listFavGifIDs: newFavs
+        });
+
+        localStorage.setItem('favourites', JSON.stringify(newFavs));
     }
 
     onSearchChangedHandler(search) {
@@ -25,7 +53,7 @@ class App extends Component {
         this.setState({
             search: true,
             loading: true,
-            gifElements: []
+            gifsData: []
         });
 
         axios.get('http://api.giphy.com/v1/gifs/search', {
@@ -35,36 +63,46 @@ class App extends Component {
                 limit: 10
             }
         })
-        .then((response) => {
-            const gifElements = response.data.data.map((gifData, index) => {
-                return <div key={`gif-${index}`}><div><span>&#9734;</span></div><img src={gifData.images.fixed_width.webp} /></div>
-            });
-
-            this.setState({
-                loading: false,
-                gifElements
+            .then((response) => {
+                this.setState({
+                    loading: false,
+                    gifsData: response.data.data,
+                })
             })
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+            .catch(function (error) {
+                console.log(error);
+            });
     }
 
     onClickClearCross() {
         this.setState({
             search: false,
             loading: false,
-            gifElements: []
+            gifsData: [],
+        });
+    }
+
+    renderGifsElement(gifsData) {
+        return gifsData.map((gifData, index) => {
+            return (
+                <Gif
+                    key={`gif-${index}`}
+                    gifId={gifData.id}
+                    gifSrc={gifData.images.fixed_width.webp}
+                    gifFav={this.state.listFavGifIDs.indexOf(gifData.id) > -1}
+                    onFavGifClickCallback={this.onFavGifClick}
+                />
+            );
         });
     }
 
     render() {
 
-        const { search, loading, gifElements } = this.state;
+        const { search, loading, gifsData } = this.state;
         const resultsCSSClassnames = cs(
             'results',
             {
-                'results-column': gifElements.length > 1
+                'results-column': gifsData.length > 1
             }
         );
 
@@ -79,8 +117,8 @@ class App extends Component {
                     <div className={resultsCSSClassnames}>
                         {!search && <img className="search-watermark" src="../assets/magnifier-512.png" />}
                         {loading && <p>Chargement en cours...</p>}
-                        {!gifElements.length && search && !loading && <p>Aucun résultats</p>}
-                        {gifElements}
+                        {!gifsData.length && search && !loading && <p>Aucun résultats</p>}
+                        {this.renderGifsElement(gifsData)}
                     </div>
                 </div>
             </div>
