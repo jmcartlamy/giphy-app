7b393b3057f081153cb9e5d49c28cc4540946e27
Jean-Marie Cart-Lamy <jean-marie.cart-lamy@fullsix.com>
2016-12-16 18:04:43 +0100

    Added active link

--- src/components/TabItem.js
+++ src/components/TabItem.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router';
 const TabItem = (props) => {
     return(
         <li className="containerTabs__tabItem">
-            <Link to={props.path}>
+            <Link to={props.path} className="containerTabs__tabItem__item" activeClassName="containerTabs__tabItem__item__active">
                 {props.name}
             </Link>
         </li>
